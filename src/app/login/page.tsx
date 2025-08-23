c76d19f26102d585def5f3d8dabd2e44c9a9542e
Nicolas Arbelaez <nicolas.arbelaez01@usc.edu.co>
2025-08-22 23:26:20 -0500

    feat: Creacion del sistema de dark mode

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,15 @@
-import { LoginForm } from "@/components/login-form"
+import { ModeToggle } from "@/components/dark-mode/mode-toggle";
+import { LoginForm } from "@/components/login-form";
 
 export default function LoginPage() {
   return (
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+      <div className="absolute top-4 right-4 md:top-8 md:right-8">
+        <ModeToggle />
+      </div>
       <div className="w-full max-w-sm md:max-w-3xl">
         <LoginForm />
       </div>
     </div>
-  )
+  );
 }
