a5d7c4e3d7016c04d1a2be8ec9a1593555ad4875
Nicolas Arbelaez <nicolas.arbelaez01@usc.edu.co>
2025-08-25 00:29:47 -0500

    fix: Añadir clase de cursor al disparador del menú desplegable en el conmutador de modo

--- src/components/dark-mode/mode-toggle.tsx
+++ src/components/dark-mode/mode-toggle.tsx
@@ -12,11 +12,11 @@ import { ThemeSelector } from "@/components/dark-mode/theme-selector";
 export function ModeToggle() {
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger asChild>
+      <DropdownMenuTrigger asChild className="cursor-pointer">
         <Button variant="outline" size="icon">
           <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
-          <span className="sr-only">Toggle theme</span>
+          <span className="sr-only">Cambiar tema</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
