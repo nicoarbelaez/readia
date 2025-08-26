bd06189fd9de70c4bc8926e1f1b9f104f08690e1
Nicolas Arbelaez <nicolas.arbelaez01@usc.edu.co>
2025-08-25 23:10:11 -0500

    refactor: Simplificar la función de inicio de sesión y mejorar la obtención de la URL base del cliente

--- src/lib/sign-in.tsx
+++ src/lib/sign-in.tsx
@@ -18,10 +18,7 @@ export const signInWithGitHub: SignInFunction = async (next?: string) => {
   return await signIn("github", next);
 };
 
-const signIn = async (
-  provider: SignInProviders,
-  next?: string,
-) => {
+const signIn = async (provider: SignInProviders, next?: string) => {
   const redirectTo = buildRedirectUrl(next);
   await supabase.auth.signInWithOAuth({
     provider: provider,
@@ -43,8 +40,13 @@ function buildRedirectUrl(next?: string): string {
 }
 
 function getBaseUrlClient() {
-  if (typeof window !== "undefined") {
-    return window.location.origin;
-  }
-  return "";
+  let url =
+    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Configura esto como la URL del sitio en el entorno de producción.
+    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Establecido automáticamente por Vercel.
+    "http://localhost:3000/";
+  // Asegúrate de incluir `https://` cuando no sea localhost.
+  url = url.startsWith("http") ? url : `https://${url}`;
+  // Asegúrate de incluir una `/` al final.
+  url = url.endsWith("/") ? url : `${url}/`;
+  return url;
 }
